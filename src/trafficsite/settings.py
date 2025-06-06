dab8f9e6f66d0d0fa5533c19335d058567b02add
zumerhu <zumerhub@gmail.com>
2025-06-06 17:50:03 +0100

    update

--- src/trafficsite/settings.py
+++ src/trafficsite/settings.py
@@ -119,8 +119,14 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trafficdb',
+        'USER': 'postgres',
+        'PASSWORD': 'zumer1234',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
 
